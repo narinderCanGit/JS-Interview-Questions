577960d69d639697f1235dbbdd3b22286b86c335
narinderCanGit <narinder111198@gmail.com>
2025-01-04 22:36:49 +0530

    Debouncing and throatling

--- debounce.js
+++ debounce.js
@@ -1,16 +1,39 @@
-let count = 0;
-function getData() {
-  console.log("Fetching Data" + count++);
+// Debounce
+const inputElement = document.getElementById('debounce');
+function getData(e) {
+  console.log("Debounce Data " + e.target.value);
 }
 
 function myDebounce(cb, d) {
   let timer;
-  return function () {
+  return function (e) {
     if (timer) {
       clearTimeout(timer);
     }
-    timer = setTimeout(cb, d);
+    timer = setTimeout(() => cb(e), d);
   };
 }
 
 const betterFunction = myDebounce(getData, 1000);
+inputElement.addEventListener('keyup', betterFunction);
+
+
+// Throttling
+const throttleElement = document.getElementById('throttle');
+ function diaplayT(event) {
+      console.log("Throttle Data:", event.target.value);
+    }
+    
+function myThrottle(fn, delay) {
+      let lastTime = 0;
+      return function(event) {
+        const now = new Date().getTime();
+        if (now - lastTime >= delay) {
+          lastTime = now;
+          fn(event);
+        }
+      }
+    }
+
+const throttleFunction = myThrottle(diaplayT, 1000);
+throttleElement.addEventListener('keyup', throttleFunction);
